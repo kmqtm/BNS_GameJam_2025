920f3ab437bec4b9bcd49c46319c61bbf9cbc4d0
inte <54321156+kmqtm@users.noreply.github.com>
2025-11-09 00:25:16 +0900

    Add CollisionManager class for collision detection
    `CollisionManager`クラスを追加し、衝突判定機能を実装しました。このクラスは、コライダーの登録、衝突結果のクリア、衝突判定の実行を行います。
    
    - `BNS_GameJam_2025.vcxproj`と`BNS_GameJam_2025.vcxproj.filters`を更新し、`CollisionManager.cpp`と`CollisionManager.h`をプロジェクトに追加。
    - `CollisionManager.cpp`に以下のメソッドを実装：
      - `RegisterCollider`, `ClearResults`, `ResolveCollisions`, `Clear`, `CheckIntersection`
    - `CollisionManager.h`にクラス定義とメソッド宣言を追加。

---
+++ src/System/CollisionManager.cpp
@@ -0,0 +1,69 @@
+﻿#include "CollisionManager.h"
+
+void CollisionManager::RegisterCollider(Collider* collider, uint32_t entity_id)
+{
+	if(collider)
+	{
+		colliders_.push_back({ collider, entity_id });
+	}
+}
+
+void CollisionManager::ClearResults()
+{
+	for(auto& entry : colliders_)
+	{
+		if(entry.collider)
+		{
+			entry.collider->ClearCollisionResult();
+		}
+	}
+}
+
+void CollisionManager::ResolveCollisions()
+{
+	const size_t count = colliders_.size();
+
+	for(size_t i = 0; i < count; ++i)
+	{
+		for(size_t j = i + 1; j < count; ++j)
+		{
+			auto& entry_a = colliders_[i];
+			auto& entry_b = colliders_[j];
+
+			if(!entry_a.collider || !entry_b.collider)
+			{
+				continue;
+			}
+
+			// 2つの Collider の shape 同士で衝突判定
+			const bool is_colliding = CheckIntersection(entry_a.collider->shape, entry_b.collider->shape);
+
+			if(is_colliding)
+			{
+				// A に B の情報を追加
+				entry_a.collider->is_colliding = true;
+				entry_a.collider->collided_tags.push_back(entry_b.collider->tag);
+
+				// B に A の情報を追加
+				entry_b.collider->is_colliding = true;
+				entry_b.collider->collided_tags.push_back(entry_a.collider->tag);
+			}
+		}
+	}
+}
+
+void CollisionManager::Clear()
+{
+	colliders_.clear();
+}
+
+bool CollisionManager::CheckIntersection(const ShapeVariant& shape1, const ShapeVariant& shape2) const
+{
+	return std::visit([&](const auto& s1)
+					  {
+						  return std::visit([&](const auto& s2)
+											{
+												return s1.intersects(s2);
+											}, shape2);
+					  }, shape1);
+}
---
+++ src/System/CollisionManager.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "../Entitie/Component/Collider.h"
+
+#include <cstdint>
+#include <Siv3D.hpp>
+#include <variant>
+#include <vector>
+
+class CollisionManager
+{
+public:
+	// Colliderとそれが属するエンティティIDを登録
+	void RegisterCollider(Collider* collider, uint32_t entity_id);
+
+	// 登録された全ての Collider の衝突結果をクリア
+	void ClearResults();
+
+	// 登録された全てのCollider同士の衝突判定を行い，結果を各 Collider に記録
+	void ResolveCollisions();
+
+	/// 登録されているColliderをすべてクリア
+	void Clear();
+
+private:
+	struct ColliderEntry
+	{
+		Collider* collider;
+		uint32_t entity_id;
+	};
+
+	std::vector<ColliderEntry> colliders_;
+
+	// 2つのShapeVariant同士の衝突判定を行う
+	bool CheckIntersection(const ShapeVariant& shape1, const ShapeVariant& shape2) const;
+};
